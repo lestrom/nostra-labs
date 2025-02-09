11c1d477b075481258ec770c2aca79d5ffaf8163
GabrielBuragev <gabstester@gmail.com>
2025-02-09 17:42:11 +0100

    pollyfil node:crypto

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,28 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import viteCompression from "vite-plugin-compression";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import viteCompression from 'vite-plugin-compression';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        viteCompression({
-            algorithm: "brotliCompress",
-            ext: ".br",
-            threshold: 1024,
-        }),
-    ],
-    clearScreen: false,
-    build: {
-        outDir: "dist",
-        minify: true,
-        cssMinify: true,
-        sourcemap: false,
-        cssCodeSplit: true,
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 1024,
+    }),
+    nodePolyfills(),
+  ],
+  clearScreen: false,
+  build: {
+    outDir: 'dist',
+    minify: true,
+    cssMinify: true,
+    sourcemap: false,
+    cssCodeSplit: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    resolve: {
-        alias: {
-            "@": "/src",
-        },
-    },
+  },
 });
